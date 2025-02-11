c3a1482f4e5462b3ef5f725e3450a95dc121ef9d
WilliamSCastro <william.sargisson.castro@gmail.com>
2025-02-11 20:18:28 +0100

    tutorial done

--- polls/urls.py
+++ polls/urls.py
@@ -13,6 +13,14 @@ from . import views
 #     path("<int:question_id>/vote/", views.vote, name="vote"),
 # ]
 
+# app_name = "polls"
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path("<int:question_id>/", views.detail, name="detail"),
+#     path("<int:question_id>/results/", views.results, name="results"),
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+# ]
+
 app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
@@ -20,4 +28,3 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
-
